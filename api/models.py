9efed1525f24c88f881062909dc6579e77f0a6de
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-10-22 17:43:59 -0500

    pequeños ajustes

--- api/models.py
+++ api/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length = 50)
-    email = models.EmailField(max_length = 100)
-    password = models.CharField(max_length = 200)
+    username = models.CharField(max_length = 50, unique=True, null=False)
+    email = models.EmailField(max_length = 100, null=False)
+    password = models.CharField(max_length = 200, null=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
 
 class Enterprise(models.Model):
     enterprise_id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey('User',on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=False)
     employers = models.IntegerField(blank=False, null=False)
     electricity = models.IntegerField(blank=False, null=False)
     gas = models.IntegerField(blank=False, null=False)
